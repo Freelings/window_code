67e070d8593e0677e34958b594405b6ceafc0333
Freelings 2020 <jianglang2017@163.com>
2020-11-19 20:59:53 +0800

    Revert "1234"
    This reverts commit 12963d9ae838542566f655aa694dfad7c39cb048.

--- EmptyProject1/EmptyProject1/firstapp.cpp
+++ EmptyProject1/EmptyProject1/firstapp.cpp
@@ -1,25 +1,7 @@
-//#include <windows.h>
-//#include <tchar.h>
-//int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInstance, char* lpCmdLine, int nShowCmd)
-//{
-//	MessageBox(NULL, _T("This is first window"), _T("This is first title"), MB_YESNO | MB_ICONINFORMATION );
-//	return 0;
-//}
-
-#include <stdio.h>
-char fun(char* c)
+#include <windows.h>
+#include <tchar.h>
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInstance, char* lpCmdLine, int nShowCmd)
 {
-    if (*c <= 'Z' && *c >= 'A') *c -= 'A' - 'a'; return *c;
-}
-main()
-{
-    char s[81], * p = s;
-    gets(s);
-    while (*p)
-    {
-        *p = fun(p);
-        putchar(*p);
-        p++;
-    }
-    printf("\n");
+	MessageBox(NULL, _T("This is first window"), _T("This is first title"), MB_YESNO | MB_ICONINFORMATION );
+	return 0;
 }
